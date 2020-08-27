8fb961dee4d7f5153055b7a7601e027cbe2293ae
Benualdo <benualdo@msn.com>
2020-08-27 23:19:39 +0200

    remove files

--- extern/VulkanMemoryAllocator/docs/html/search/pages_7.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['quick_20start_492',['Quick start',['../quick_start.html',1,'index']]]
-];
--- extern/VulkanMemoryAllocator/docs/html/search/pages_9.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['statistics_496',['Statistics',['../statistics.html',1,'index']]],
-  ['staying_20within_20budget_497',['Staying within budget',['../staying_within_budget.html',1,'index']]]
-];
