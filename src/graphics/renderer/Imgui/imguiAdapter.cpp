f9e92bcf9f479d1dd4cec59abfed1b431a1b6b5b
Benualdo <benualdo@msn.com>
2021-05-24 21:55:56 +0200

    ImGui style update

--- src/graphics/renderer/Imgui/imguiAdapter.cpp
+++ src/graphics/renderer/Imgui/imguiAdapter.cpp
@@ -34,7 +34,7 @@ namespace vg::graphics::renderer
         ImGuiStyle & style = ImGui::GetStyle();
         ImVec4 * colors = style.Colors;
 
-        style.PopupRounding = 3;
+        style.PopupRounding = 4;
         style.WindowPadding = ImVec2(4, 4);
         style.FramePadding = ImVec2(4, 4);
         style.ItemSpacing = ImVec2(4, 4);
@@ -50,7 +50,7 @@ namespace vg::graphics::renderer
         style.FrameRounding = 4;
         style.ScrollbarRounding = 4;
         style.GrabRounding = 4;
-        style.Alpha = 0.9f;
+        style.Alpha = 1.0f;
 
         #ifdef IMGUI_HAS_DOCK 
         style.TabBorderSize = 0;
@@ -62,20 +62,20 @@ namespace vg::graphics::renderer
         colors[ImGuiCol_WindowBg]               = ImVec4(0.18f, 0.19f, 0.21f, 1.00f);
         colors[ImGuiCol_ChildBg]                = ImVec4(0.18f, 0.19f, 0.21f, 1.00f);
         colors[ImGuiCol_PopupBg]                = ImVec4(0.18f, 0.19f, 0.21f, 1.00f);
-        colors[ImGuiCol_Border]                 = ImVec4(0.12f, 0.12f, 0.12f, 0.71f);
+        colors[ImGuiCol_Border]                 = ImVec4(0.13f, 0.13f, 0.14f, 1.00f);
         colors[ImGuiCol_BorderShadow]           = ImVec4(1.00f, 1.00f, 1.00f, 0.06f);
-        colors[ImGuiCol_FrameBg]                = ImVec4(0.25f, 0.27f, 0.29f, 0.54f);
-        colors[ImGuiCol_FrameBgHovered]         = ImVec4(0.42f, 0.42f, 0.42f, 0.40f);
-        colors[ImGuiCol_FrameBgActive]          = ImVec4(0.56f, 0.56f, 0.56f, 0.67f);
+        colors[ImGuiCol_FrameBg]                = ImVec4(0.25f, 0.27f, 0.29f, 1.00f);
+        colors[ImGuiCol_FrameBgHovered]         = ImVec4(0.42f, 0.42f, 0.42f, 1.00f);
+        colors[ImGuiCol_FrameBgActive]          = ImVec4(0.56f, 0.56f, 0.56f, 1.00f);
         colors[ImGuiCol_TitleBg]                = ImVec4(0.13f, 0.13f, 0.14f, 1.00f);
-        colors[ImGuiCol_TitleBgActive]          = ImVec4(0.35f, 0.40f, 0.95f, 1.00f);
+        colors[ImGuiCol_TitleBgActive]          = ImVec4(0.25f, 0.27f, 0.29f, 1.00f);
         colors[ImGuiCol_TitleBgCollapsed]       = ImVec4(0.13f, 0.13f, 0.14f, 0.50f);
         colors[ImGuiCol_MenuBarBg]              = ImVec4(0.13f, 0.13f, 0.14f, 1.00f);
         colors[ImGuiCol_ScrollbarBg]            = ImVec4(0.18f, 0.19f, 0.21f, 0.53f);
         colors[ImGuiCol_ScrollbarGrab]          = ImVec4(0.13f, 0.13f, 0.14f, 1.00f);
         colors[ImGuiCol_ScrollbarGrabHovered]   = ImVec4(0.35f, 0.40f, 0.95f, 1.00f);
         colors[ImGuiCol_ScrollbarGrabActive]    = ImVec4(0.93f, 0.26f, 0.27f, 1.00f);
-        colors[ImGuiCol_CheckMark]              = ImVec4(0.74f, 0.65f, 0.65f, 1.00f);
+        colors[ImGuiCol_CheckMark]              = ImVec4(1.00f, 1.00f, 1.00f, 0.50f);
         colors[ImGuiCol_SliderGrab]             = ImVec4(0.35f, 0.40f, 0.95f, 1.00f);
         colors[ImGuiCol_SliderGrabActive]       = ImVec4(0.93f, 0.26f, 0.27f, 1.00f);
         colors[ImGuiCol_Button]                 = ImVec4(0.28f, 0.32f, 0.77f, 1.00f);
@@ -83,20 +83,20 @@ namespace vg::graphics::renderer
         colors[ImGuiCol_ButtonActive]           = ImVec4(0.93f, 0.26f, 0.27f, 1.00f);
         colors[ImGuiCol_Header]                 = ImVec4(0.28f, 0.32f, 0.77f, 1.00f);
         colors[ImGuiCol_HeaderHovered]          = ImVec4(0.35f, 0.40f, 0.95f, 1.00f);
-        colors[ImGuiCol_HeaderActive]           = ImVec4(0.93f, 0.26f, 0.27f, 1.00f);
+        colors[ImGuiCol_HeaderActive]           = ImVec4(0.35f, 0.40f, 0.95f, 1.00f);
         colors[ImGuiCol_Separator]              = ImVec4(0.13f, 0.13f, 0.14f, 0.50f);
         colors[ImGuiCol_SeparatorHovered]       = ImVec4(0.28f, 0.32f, 0.77f, 1.00f);
-        colors[ImGuiCol_SeparatorActive]        = ImVec4(0.93f, 0.26f, 0.27f, 1.00f);
+        colors[ImGuiCol_SeparatorActive]        = ImVec4(0.35f, 0.40f, 0.95f, 1.00f);
         colors[ImGuiCol_ResizeGrip]             = ImVec4(0.28f, 0.32f, 0.77f, 1.00f);
         colors[ImGuiCol_ResizeGripHovered]      = ImVec4(0.35f, 0.40f, 0.95f, 1.00f);
         colors[ImGuiCol_ResizeGripActive]       = ImVec4(0.93f, 0.26f, 0.27f, 1.00f);
         colors[ImGuiCol_Tab]                    = ImVec4(0.28f, 0.32f, 0.77f, 1.00f);
         colors[ImGuiCol_TabHovered]             = ImVec4(0.35f, 0.40f, 0.95f, 1.00f);
-        colors[ImGuiCol_TabActive]              = ImVec4(0.93f, 0.26f, 0.27f, 1.00f);
+        colors[ImGuiCol_TabActive]              = ImVec4(0.35f, 0.40f, 0.95f, 1.00f);
         colors[ImGuiCol_TabUnfocused]           = ImVec4(0.28f, 0.32f, 0.77f, 0.50f);
-        colors[ImGuiCol_TabUnfocusedActive]     = ImVec4(0.93f, 0.26f, 0.27f, 0.50f);
-        colors[ImGuiCol_DockingPreview]         = ImVec4(0.85f, 0.85f, 0.85f, 0.28f);
-        colors[ImGuiCol_DockingEmptyBg]         = ImVec4(0.38f, 0.38f, 0.38f, 1.00f);
+        colors[ImGuiCol_TabUnfocusedActive]     = ImVec4(0.35f, 0.40f, 0.95f, 0.50f);
+        colors[ImGuiCol_DockingPreview]         = ImVec4(0.25f, 0.27f, 0.29f, 1.00f);
+        colors[ImGuiCol_DockingEmptyBg]         = ImVec4(0.13f, 0.13f, 0.14f, 1.00f);
         colors[ImGuiCol_PlotLines]              = ImVec4(0.61f, 0.61f, 0.61f, 1.00f);
         colors[ImGuiCol_PlotLinesHovered]       = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
         colors[ImGuiCol_PlotHistogram]          = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
@@ -119,14 +119,10 @@ namespace vg::graphics::renderer
     {
         ImGui::CreateContext();
         ImGuiIO & io = ImGui::GetIO();
-        // TODO: Set optional io.ConfigFlags values, e.g. 'io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard' to enable keyboard controls.
-        // TODO: Fill optional fields of the io structure later.
-        // TODO: Load TTF/OTF fonts if you don't want to use the default font.
 
         io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard | ImGuiConfigFlags_DockingEnable;
         io.ConfigDockingTransparentPayload = true;
 
-        //ImGui::StyleColorsDark();
         FiftyShadesOfGreyStyle();
 
         io.Fonts->AddFontFromFileTTF("data/Fonts/ubuntu-mono/UbuntuMono-R.ttf", 16);
