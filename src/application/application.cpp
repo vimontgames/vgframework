6b1c834f092bd2862003dea8fd59207d0b09477b
Benualdo <benualdo@msn.com>
2024-10-02 23:14:47 +0200

    Version 0.34

--- src/application/application.cpp
+++ src/application/application.cpp
@@ -5,7 +5,7 @@
 namespace vg
 {
     #define VG_VERSION_MAJOR 0
-    #define VG_VERSION_MINOR 33
+    #define VG_VERSION_MINOR 34
 
     //--------------------------------------------------------------------------------------
     core::IPlugin::Version Application::GetVersion() const
