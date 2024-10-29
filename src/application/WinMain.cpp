e0c84cbc00883b77df1721c7e33cc5039b376ede
Benualdo <benualdo@msn.com>
2024-10-29 18:43:21 +0100

    Update Editor.exe workflow

--- src/application/WinMain.cpp
+++ src/application/WinMain.cpp
@@ -170,7 +170,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 	#ifdef _CRTDBG_MAP_ALLOC
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	//_crtBreakAlloc = 159;
+	//_crtBreakAlloc = 0;
 	#endif
 
 	// Save Startup root directory
