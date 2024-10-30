412f680f8d881cfab056271b1bf7bb3729537aaa
Benualdo <benualdo@msn.com>
2024-10-30 01:36:09 +0100

    Update generate_version_header.bat

--- generate_version_header.bat
+++ generate_version_header.bat
@@ -1,4 +1,4 @@
-rem @echo off
+@echo off
 
 rem Change to the directory where your header should be generated
 cd
@@ -8,7 +8,6 @@ rem Get the current short Git revision
 for /f "delims=" %%i in ('git rev-parse --short HEAD') do set GIT_REVISION=%%i
 
 rem Create the version header file
-echo // This file is generated automatically during build > version.h
 echo #define GIT_REVISION "%GIT_REVISION%" >> version.h
 
 rem Optional: print a message
