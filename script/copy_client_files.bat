181bce472175c26461f5294fa04e7641ee65097a
Benualdo <benualdo@msn.com>
2025-11-03 22:39:11 +0100

    Fix caps in editor.exe and game.exe and remove unnecessary copy

--- script/copy_client_files.bat
+++ script/copy_client_files.bat
@@ -1,7 +1,5 @@
-copy /y "vgframework_win64_msvc_dx12_release.exe" "Editor.exe"
 xcopy "build\bin\Win64\release MSVC" "bin\Win64\release MSVC" /E /H /C /I /Y
 xcopy "build\bin\Win64\release MSVC DX12" "bin\Win64\release MSVC DX12" /E /H /C /I /Y
 
-copy /y "vgframework_win64_msvc_dx12_final.exe" "Game.exe"
 xcopy "build\bin\Win64\final MSVC" "bin\Win64\final MSVC" /E /H /C /I /Y
 xcopy "build\bin\Win64\final MSVC DX12" "bin\Win64\final MSVC DX12" /E /H /C /I /Y
