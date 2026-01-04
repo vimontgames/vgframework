c8ce3058eae86e711024045ae4dd07e390f3a76a
Benualdo <benualdo@msn.com>
2026-01-04 07:51:46 +0100

    Make picking easier by ignoring pixels with no picking ID (e.g. wireframe)

--- data/Shaders/system/picking.hlsl
+++ data/Shaders/system/picking.hlsl
@@ -4,7 +4,7 @@
 //--------------------------------------------------------------------------------------
 bool ProcessPicking(uint _rwBufferID, uint _offset, uint2 _inputPos, float _depth, float3 _worldPos, uint2 _mousePos, uint2 _screenSize, uint4 _pickingID)
 {
-    if (0xFFFF != _rwBufferID)
+    if (0xFFFF != _rwBufferID && 0 != _pickingID.x)
     {        
         if (all(_mousePos.xy == _inputPos.xy) && all(_mousePos.xy < _screenSize))
         {
